bcd670cedcf62a166569b6fe474997bff9726e71
ilyadev750 <petrovich76007600kz@gmail.com>
2024-04-07 15:56:33 +0200

    I I figured out how to add span style to content

--- dictionaries/views.py
+++ dictionaries/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from .models import Dictionary
 from lections.models import Lection
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
+import re
 
 
 def get_lection_dictionary(request, lection_slug):
@@ -14,10 +16,15 @@ def get_lection_dictionary(request, lection_slug):
     
     values = Dictionary.objects.filter(lection_id=lection)
 
+    # red = '(1)STYLESTYleSTYLE(1)(1)STYLESTYleSTYLE(1)(1)STYLESTYleSTYLE(1)'
+    # pattern_1 = "\D1\D\w+\D1\D"
+    # red = re.sub(pattern_1, '<span class="red">JURASSIC PARK</span>', red)
+    # red = mark_safe(red)
+
     context = {
         'values': values,
         'lection': lection,
-        'red': '<span id="red">Красный</span>'
+        # 'red': red,
     }
     
     return render(request, 'dictionaries/dictionary.html', context)
