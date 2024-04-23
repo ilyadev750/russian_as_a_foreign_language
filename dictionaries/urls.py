dae8af2aef02b31a1fa6c68dbf3b9b3ccab0483e
ilyadev750 <petrovich76007600kz@gmail.com>
2024-04-23 14:34:51 +0200

    dictionary is working

--- dictionaries/urls.py
+++ dictionaries/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_lection_dictionary, create_new_dictionary
+from .views import get_lection_dictionary, create_new_dictionary, delete_dictionary_item
 
 urlpatterns = [
-    path('<slug:lection_slug>/create-dictionary', create_new_dictionary, name='create_new_dictionary'),
-    path('<slug:lection_slug>/dictionary', get_lection_dictionary, name='get_lection_dictionary'),
+    path('<slug:lection_slug>/create-dictionary',
+          create_new_dictionary,
+          name='create_new_dictionary'),
+    path('<slug:lection_slug>/dictionary',
+         get_lection_dictionary,
+         name='get_lection_dictionary'),
+    path('<slug:lection_slug>/dictionary/delete',
+         delete_dictionary_item,
+         name='delete_dictionary_item')
 
 ]
--- dictionaries/views.py
+++ dictionaries/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.utils.safestring import mark_safe
 from .models import Dictionary
+from django.urls import reverse
 from lections.models import Lection
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
@@ -21,10 +22,14 @@ def get_lection_dictionary(request, lection_slug):
     # pattern_1 = "(\D1\D\w+\D1\D)"
     # red = re.sub(pattern_1, '<span id="red">\\1</span>', red)
     # red = mark_safe(red)
-
+    if request.user.is_superuser:
+        delete_dict_item_url = reverse('delete_dictionary_item', args=[lection_slug])
+    else:
+        delete_dict_item_url = ''
     context = {
         'values': values,
         'lection': lection,
+        'delete_dict_item_url': delete_dict_item_url,
         # 'red': red,
     }
     
@@ -38,14 +43,25 @@ def create_new_dictionary(request, lection_slug):
         if formset.is_valid():
             lection_id = Lection.objects.get(slug=lection_slug)
             for form in formset:
-                dictionary_obj = Dictionary(
-                    termin_in_rus=form.cleaned_data['termin_in_rus'],
-                    termin_in_eng=form.cleaned_data['termin_in_eng'],
-                    lection_id=lection_id
-                )
-                dictionary_obj.save()
-                return redirect('home')
+                try:
+                    dictionary_obj = Dictionary(
+                        termin_in_rus=form.cleaned_data['termin_in_rus'],
+                        termin_in_eng=form.cleaned_data['termin_in_eng'],
+                        lection_id=lection_id
+                    )
+                    dictionary_obj.save()
+                except KeyError:
+                    break
+            return redirect('home')
     context = {
         'formset': DictionaryFormset(queryset=Dictionary.objects.none())
     }
     return render(request, 'dictionaries/create_new_dictionary.html', context)
+
+
+def delete_dictionary_item(request, lection_slug):
+    if request.user.is_superuser:
+        pk = request.GET.get('pk')
+        dictionary = Dictionary.objects.get(pk=int(pk))
+        dictionary.delete()
+        return redirect('get_lection_dictionary', lection_slug)
