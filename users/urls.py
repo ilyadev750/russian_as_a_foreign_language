71efed3132db640b161d5c64f79066d263e206b2
ilyadev750 <petrovich76007600kz@gmail.com>
2024-05-09 18:23:01 +0200

    working with templates

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
-from django.urls import re_path, path
-from .views import signup, activate
+from django.urls import path
+from .views import signup, activate, login_user, logout_user
 
 
 urlpatterns = [
-    # re_path(r'^signup/$', signup, name='signup'),
-    # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     activate, name='activate'),
     path('signup/', signup, name='signup'),
-    path('activate/<uidb64>/<token>/', activate, name='activate')
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('login/', login_user, name="login_user"),
+    path('logout/', logout_user, name="logout_user")
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from .forms import SignupForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
 from django.core.mail import send_mail
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 
 def signup(request):
@@ -19,7 +24,6 @@ def signup(request):
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            # current_site = get_current_site(request)
             mail_subject = 'Активируйте ваш аккаунт!'
             message = render_to_string('users/activation_email.html', {
                 'user': user,
@@ -28,17 +32,18 @@ def signup(request):
                 'token':account_activation_token.make_token(user),
             })
             to_email = form.cleaned_data.get('email')
-            print(message)
-
             status = send_mail(
                 subject=mail_subject,
                 message=mail_subject,
-                from_email='petrovich7600@yandex.ru',
+                from_email=os.getenv('EMAIL_HOST_USER'),
                 recipient_list=[to_email],
                 html_message=message,
             )
             print(status)
-            return HttpResponse('Пожалуйста, подвтердите вашу электронную почту для завершения регистрации!')
+            context = {
+                'activation': 'Пожалуйста, подвтердите вашу электронную почту для завершения регистрации!'
+                }
+            return render(request, 'users/send_activation_link.html', context=context)
     else:
         form = SignupForm()
     return render(request, 'users/signup.html', {'form': form})
@@ -53,8 +58,34 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        # login(request, user)
-        # return redirect('home')
-        return HttpResponse('Спасибо за активацию почты. Теперь Вы можете зайти в личный кабинет!')
+        context = {
+            'success_activation': 'Спасибо за активацию почты. Теперь Вы можете зайти в личный кабинет!' 
+            }
+        return render(request, 'users/activation_successfull.html', context=context)
     else:
         return HttpResponse('Неверная ссылка!')
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            form = AuthenticationForm()
+            context = {
+                "form": form,
+                "error": 'Неверный пользователь или пароль!'
+                }
+            return render(request, "users/login.html", context)
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form, "error": ''})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("home")
